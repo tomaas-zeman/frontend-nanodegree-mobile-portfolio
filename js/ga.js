fe1ed09b590b3de53494846ccd8f0e6c46d73675
Tomas Zeman <tomas.zeman@showmax.com>
2018-08-17 12:56:53 +0200

    Index optimizations

---
+++ js/ga.js
@@ -0,0 +1,6 @@
+function ga (w,g) {w['GoogleAnalyticsObject']=g;w[g]=w[g]||function(){(w[g].q=w[g].q||[]).push(arguments)};w[g].l=1*new Date();};
+
+window.addEventListener("load", function(event) {
+    ga('create', 'UA-XXXX-Y');
+    ga('send', 'pageview')
+  });
